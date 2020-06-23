f39e609d0d77484115123230959fd4afe12b069d
atlwendy <atlwendy@gmail.com>
2020-06-23 13:13:34 -0400

    cohort date range

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TsInputModule } from '@terminus/ui-input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TsCohortDateRangeModule } from '@terminus/ui-cohort-date-range';
+import { TsDateRangeModule } from '@terminus/ui-date-range';
+import { TsSelectionListModule } from '@terminus/ui-selection-list';
+import { TsOptionModule } from '@terminus/ui-option';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    TsInputModule,
+    TsCohortDateRangeModule,
+    TsDateRangeModule,
+    TsSelectionListModule,
+    TsOptionModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
